268574997023b3a5ced796cb8e4b4bf6fad51b45
Renan Sampaio <31661482+renanmsampaio@users.noreply.github.com>
2025-04-10 15:38:39 +0100

    Update index.js

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch').default;
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
     'admin': 'b1e3ddb4-8eba-412c-bd7b-f5257a6ed926',
-    'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
+//    'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
 };
 
 module.exports = async function (context, req) {
